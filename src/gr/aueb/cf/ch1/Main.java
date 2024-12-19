44d04a1c0a2f48f85d4b60d1d543b9c48eb7f290
Evangelos. Vatamidis <vatamidis_vaggelis@yahoo.gr>
2024-12-19 12:56:52 +0200

    hotflix completed. Try catch added for Arithmetic exception

--- src/gr/aueb/cf/ch1/Main.java
+++ src/gr/aueb/cf/ch1/Main.java
@@ -14,5 +14,15 @@ public class Main {
         return a - b;
     }
 
-    public  static int div(int a, int b){return a/b;}
+    public  static int div(int a, int b){
+
+        try{
+            return a/b;
+        }catch(ArithmeticException e){
+            System.err.println("Error. Denominator must not be zero");
+            throw e;
+        }
+}
+
+
 }
